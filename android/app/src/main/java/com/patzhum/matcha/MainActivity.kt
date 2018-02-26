aadb97c11898e290663ad5bc57c665b5f870b9f7
patzhum <patrick.hum@hotmail.com>
2018-02-26 00:53:23 -0500

    Merge conflict resolution

--- android/app/src/main/java/com/patzhum/matcha/MainActivity.kt
+++ android/app/src/main/java/com/patzhum/matcha/MainActivity.kt
@@ -4,13 +4,9 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
-<<<<<<< HEAD
 import android.util.MalformedJsonException
 import android.view.View
-=======
-import android.view.View
-import android.view.ViewGroup
->>>>>>> This fixes a UI issue where new Activities were being created.
+
 import android.widget.LinearLayout
 import android.widget.TextView
 import com.google.firebase.auth.FirebaseAuth
