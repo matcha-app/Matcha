57243e23bd4b1ca76b1f52a8d1c2f5a220241455
edwinlo <e8lo@edu.uwaterloo.ca>
2018-04-06 17:47:47 -0400

    Added more functionality for Textview

--- android/app/src/main/java/com/patzhum/matcha/render/MatchaTextView.kt
+++ android/app/src/main/java/com/patzhum/matcha/render/MatchaTextView.kt
@@ -1,6 +1,13 @@
 package com.patzhum.matcha.render
 
 import android.content.Context
+import android.content.res.ColorStateList
+import android.content.res.Resources
+import android.os.Build
+import android.support.v4.widget.TextViewCompat
+import android.text.method.DigitsKeyListener
+import android.text.method.PasswordTransformationMethod
+import android.text.method.QwertyKeyListener
 import android.widget.TextView
 
 /**
@@ -17,18 +24,226 @@ class MatchaTextView : MatchaView<TextView, MatchaTextView.State>() {
     override fun render(view : TextView, state : State) {
         super.render(view, state)
         view.apply {
-            text = state.text
+            autoLinkMask = RenderUtil.tryParseAutolink(state.autolink, autoLinkMask)
+            if (Build.VERSION.SDK_INT > 26) {
+                setAutoSizeTextTypeUniformWithConfiguration(
+                        RenderUtil.tryParseInt(state.autoSizeMinTextSize, autoSizeMinTextSize),
+                        RenderUtil.tryParseInt(state.autoSizeMaxTextSize, autoSizeMaxTextSize),
+                        RenderUtil.tryParseInt(state.autoSizeStepGranularity, autoSizeStepGranularity),
+                        RenderUtil.tryParseAutoSizeTextType(state.autoSizeTextType, autoSizeTextType)
+                )
+                //need to implement @Arrays/resources
+                setAutoSizeTextTypeUniformWithPresetSizes(
+                        intArrayOf(RenderUtil.tryParseInt(state.autoSizePresetSizes)),
+                        RenderUtil.tryParseAutoSizeTextType(state.autoSizeTextType, autoSizeTextType))
+            }
+            //autotext
+            keyListener = QwertyKeyListener.getInstance(RenderUtil.tryParseBoolean(state.autoText),RenderUtil.tryParseCaptialize(state.capitalize))
+
+            if (Build.VERSION.SDK_INT  >23) {
+                breakStrategy = RenderUtil.tryParseBreakStrategy(state.breakStrategy, breakStrategy)
+                compoundDrawableTintList = ColorStateList.valueOf(RenderUtil.tryParseColor(state.drawableTint, compoundDrawableTintList.defaultColor))
+                compoundDrawableTintMode = RenderUtil.tryParseDrawableTintMode(state.drawableTintMode, compoundDrawableTintMode)
+                hyphenationFrequency = RenderUtil.tryParseHyphenationFrequency(state.hyphenationFrequency, hyphenationFrequency)
+
+            }
+
+            isCursorVisible = RenderUtil.tryParseBoolean(state.cursorVisible)
+            keyListener = DigitsKeyListener.getInstance(state.digits) //digits
+
+
+            setCompoundDrawablesWithIntrinsicBounds(
+                    RenderUtil.tryParseResourceId(state.drawableLeft),
+                    RenderUtil.tryParseResourceId(state.drawableTop),
+                    RenderUtil.tryParseResourceId(state.drawableRight),
+                    RenderUtil.tryParseResourceId(state.drawableBottom)
+            )
+
+            setCompoundDrawablesRelativeWithIntrinsicBounds(
+                    RenderUtil.tryParseResourceId(state.drawableStart),
+                    RenderUtil.tryParseResourceId(state.drawableTop),
+                    RenderUtil.tryParseResourceId(state.drawableEnd),
+                    RenderUtil.tryParseResourceId(state.drawableBottom)
+            )
+
+            compoundDrawablePadding = RenderUtil.tryParseInt(state.drawablePadding)
+            //editableText
+
+            if (Build.VERSION.SDK_INT > 21) {
+                setElegantTextHeight(RenderUtil.tryParseBoolean(state.elegantTextHeight))
+                fontFeatureSettings = state.fontFeatureSettings
+                letterSpacing = RenderUtil.tryParseFloat(state.letterSpacing, letterSpacing)
+            }
+
+            when (RenderUtil.tryParseBoolean(state.editable)) {
+                true -> {
+                    isCursorVisible = true
+                    isEnabled = true
+                    isFocusableInTouchMode = true
+                    requestFocus()
+                }
+                false -> {
+                    isCursorVisible = false
+                    isEnabled = false
+                    isFocusableInTouchMode = false
+                }
+            }
+
+            ellipsize = RenderUtil.tryParseEllipsize(state.ellipsize, ellipsize)
+            setEms(RenderUtil.tryParseInt(state.ems))
+
+            freezesText = RenderUtil.tryParseBoolean(state.freezesText, freezesText)
+            gravity = RenderUtil.tryParseGravity(state.gravity, gravity)
+            height = RenderUtil.tryParseInt(state.height, height)
+            hint = state.hint
+            setImeActionLabel(state.imeActionLabel,RenderUtil.tryParseInt(state.imeActionId, imeActionId))
+            imeOptions = RenderUtil.tryParseImeOptions(state.imeOptions, imeOptions)
+            includeFontPadding = RenderUtil.tryParseBoolean(state.includeFontPadding, includeFontPadding)
+            //inputmethod needed
+
+            setRawInputType(RenderUtil.tryParseInputType(state.inputType))
+            setLineSpacing(
+                    RenderUtil.tryParseFloat(state.lineSpacingExtra,lineSpacingExtra),
+                    RenderUtil.tryParseFloat(state.lineSpacingMultiplier, lineSpacingMultiplier)
+            )
+
+            setLines(RenderUtil.tryParseInt(state.lines))
+            linksClickable = RenderUtil.tryParseBoolean(state.linksClickable, linksClickable)
+            marqueeRepeatLimit = RenderUtil.tryParseMarqueeRepeatLimit(state.marqueeRepeatLimit, marqueeRepeatLimit)
+            maxEms = RenderUtil.tryParseInt(state.maxEms, maxEms)
+            maxHeight = RenderUtil.tryParseInt(state.maxHeight, maxHeight)
+            //maxLength = RenderUtil.tryParseInt(state.maxLength, maxLength)
+            filters = RenderUtil.tryParseInputFilter(state.maxLength)
+            maxLines = RenderUtil.tryParseInt(state.maxLines, maxLines)
+            maxWidth = RenderUtil.tryParseInt(state.maxWidth, maxWidth)
+            minEms = RenderUtil.tryParseInt(state.minEms, minEms)
+            minHeight = RenderUtil.tryParseInt(state.minHeight, minHeight)
+            minLines = RenderUtil.tryParseInt(state.minLines, minLines)
+            minWidth = RenderUtil.tryParseInt(state.minWidth, minWidth)
+
+            //numeric
+            inputType = RenderUtil.tryParseInputType(state.numeric)
+            inputType = inputType.or(RenderUtil.tryParseInputType(state.phoneNumber))
+
+            setHorizontallyScrolling(RenderUtil.tryParseBoolean(state.scrollHorizontally))
+            setSelectAllOnFocus(RenderUtil.tryParseBoolean(state.selectAllOnFocus))
+            setShadowLayer(
+                    RenderUtil.tryParseFloat(state.shadowRadius, shadowRadius),
+                    RenderUtil.tryParseFloat(state.shadowDx, shadowDx),
+                    RenderUtil.tryParseFloat(state.shadowDy, shadowDy),
+                    RenderUtil.tryParseColor(state.shadowColor, shadowColor)
+            )
+
+            setSingleLine(RenderUtil.tryParseBoolean(state.singleLine))
+            transformationMethod = when (RenderUtil.tryParseBoolean(state.password)){
+                true -> PasswordTransformationMethod()
+                false -> null
+            }
+
+            setText(state.text, RenderUtil.tryParseBufferType(state.bufferType))
+            setTextAppearance(context, RenderUtil.tryParseResourceId(state.textAppearance))
             setTextColor(RenderUtil.tryParseColor(state.textColor, currentTextColor))
+            setHintTextColor(RenderUtil.tryParseColor(state.textColorHint, currentHintTextColor))
+            setLinkTextColor(RenderUtil.tryParseColor(state.textColorLink))
+            highlightColor = RenderUtil.tryParseColor(state.textColorHighlight)
+
+            setAllCaps(RenderUtil.tryParseBoolean(state.textAllCaps))
+
             textSize = RenderUtil.tryParseFloat(state.textSize, textSize)
             textAlignment = RenderUtil.tryParseTextAlignment(state.textAlignment, textAlignment)
+            setTextIsSelectable(RenderUtil.tryParseBoolean(state.textIsSelectable))
+            textScaleX = RenderUtil.tryParseFloat(state.textScaleX, textScaleX)
+            typeface = RenderUtil.tryParseTypeface(state.typeface, RenderUtil.tryParseTextStyle(state.textStyle))
+            width = RenderUtil.tryParseInt(state.width, width)
         }
     }
 
     class State(
+            val autolink : String? = null,
+            val autoSizeMaxTextSize : String? = null,
+            val autoSizeMinTextSize : String? = null,
+            val autoSizePresetSizes : String? = null,
+            val autoSizeStepGranularity : String? = null,
+            val autoSizeTextType : String? = null,
+            val autoText : String? = null,
+
+            val breakStrategy : String? = null,
+            val bufferType : String? = null,
+            val capitalize : String? = null,
+            val cursorVisible : String? = null,
+
+            val digits : String? = null,
+            val drawableBottom : String? = null,
+            val drawableEnd : String? = null,
+            val drawableLeft : String? = null,
+            val drawablePadding : String? = null,
+            val drawableRight : String? = null,
+            val drawableStart : String? = null,
+            val drawableTint : String? = null,
+            val drawableTintMode : String? = null,
+            val drawableTop : String? = null,
+            val editable : String? = null,
+            val editorExtras : String? =  null,
+            val elegantTextHeight : String? = null,
+            val ellipsize: String? =  null,
+            val ems: String? =  null,
+            val fontFamily : String? =  null,
+            val fontFeatureSettings : String? =  null,
+            val freezesText : String? =  null,
+            val gravity : String? =  null,
+            val height : String? =  null,
+            val hint : String? =  null,
+            val hyphenationFrequency : String? =  null,
+            val imeActionId : String? =  null,
+            val imeActionLabel : String? =  null,
+            val imeOptions : String? =  null,
+            val includeFontPadding : String? =  null,
+            val inputMethod : String? =  null,
+            val inputType: String? =  null,
+            val letterSpacing : String? =  null,
+            val lineSpacingExtra : String? =  null,
+            val lineSpacingMultiplier : String? =  null,
+            val lines : String? =  null,
+            val linksClickable : String? =  null,
+            val marqueeRepeatLimit : String? =  null,
+            val maxEms : String? =  null,
+            val maxHeight : String? =  null,
+            val maxLength: String? =  null,
+            val maxLines: String? =  null,
+            val maxWidth : String? =  null,
+            val minEms : String? =  null,
+            val minHeight : String? =  null,
+            val minLines : String? =  null,
+            val minWidth : String? =  null,
+            val numeric : String? =  null,
+            val password : String? =  null,
+            val phoneNumber : String? =  null,
+            val privateImeOptions : String? =  null,
+            val scrollHorizontally : String? =  null,
+            val selectAllOnFocus : String? =  null,
+            val shadowColor : String? =  null,
+            val shadowDx : String? =  null,
+            val shadowDy : String? =  null,
+            val shadowRadius : String? =  null,
+            val singleLine : String? =  null,
+
             val text : String? = null,
+            val textAllCaps : String? =  null,
+            val textAppearance : String? =  null,
+
             val textColor: String? = null,
+            val textColorHighlight: String? = null,
+
+            val textColorHint : String? =  null,
+            val textColorLink : String? =  null,
+
             val textSize: String? = null,
-            val textAlignment: String? = null
+            val textAlignment: String? = null,
+            val textIsSelectable: String? = null,
+            val textScaleX: String? = null,
+            val textStyle: String? = null,
+            val typeface: String? = null,
+            val width: String? = null
     ) : MatchaView.State(type = TextView::class.java.simpleName)
 }
 
--- android/app/src/main/java/com/patzhum/matcha/render/RenderUtil.kt
+++ android/app/src/main/java/com/patzhum/matcha/render/RenderUtil.kt
@@ -1,14 +1,25 @@
 package com.patzhum.matcha.render
 
 import android.content.Context
+import android.content.res.Resources
 import android.graphics.Color
+import android.graphics.PorterDuff
+import android.graphics.Typeface
 import android.os.Build
+import android.text.InputFilter
+import android.text.InputType
+import android.text.TextUtils
+import android.text.TextUtils.split
+import android.text.method.TextKeyListener
 
 import android.util.Log
+import android.view.Gravity
 import android.view.View
 import android.view.ViewGroup
 import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.view.inputmethod.EditorInfo
+import android.widget.TextView
 import com.google.gson.Gson
 import com.google.gson.JsonSyntaxException
 import com.google.gson.stream.MalformedJsonException
@@ -19,6 +30,31 @@ import com.google.gson.stream.MalformedJsonException
 class RenderUtil {
     companion object {
         private val LOG_TAG = RenderUtil::class.java.simpleName
+
+        private val AUTOLINK_ALL = 15
+        private val AUTOLINK_MAP = 8
+        private val AUTOLINK_PHONE = 4
+        private val AUTOLINK_EMAIL = 2
+        private val AUTOLINK_WEB = 1
+        private val AUTOLINK_NONE = 0
+
+        private val AUTOSIZETEXTYPE_NONE = 0
+        private val AUTOSIZETEXTTYPE_UNIFORM = 1
+
+        private val BREAKSTRATEGY_BALANCED = 2
+        private val BREAKSTRATEGY_HIGH_QUAL = 1
+        private val BREAKSTRATEGY_SIMPLE = 0
+
+        private val CAPITALIZE_CHARS = 3
+        private val CAPITALIZE_WORDS = 2
+        private val CAPITALIZE_SENTENCES = 1
+        private val CAPITALIZE_NONE = 0
+
+        private val HYPHENATIONFREQ_FULL = 2
+        private val HYPHENATIONFREQ_NORMAL = 1
+        private val HYPHENATIONFREQ_NONE = 0
+
+        private val MARQUEEREPEAT_FOREVER = -1
 
         fun <ViewType: View, StateType: MatchaView.State, MatchaViewType: MatchaView<ViewType, StateType>>
                 renderView(
@@ -76,6 +112,10 @@ class RenderUtil {
             return string.toIntOrNull() ?: defaultValue
         }
 
+        fun tryParseBoolean(string: String?, defaultValue: Boolean = false) : Boolean{
+            string ?: return defaultValue
+            return string.toBoolean()
+        }
 
         fun tryParseLayoutSize(string : String?, defaultValue: Int = WRAP_CONTENT) : Int {
             string ?: return defaultValue
@@ -96,6 +136,246 @@ class RenderUtil {
                 "textEnd" -> View.TEXT_ALIGNMENT_TEXT_END
                 "viewStart" -> View.TEXT_ALIGNMENT_VIEW_START
                 "viewEnd" -> View.TEXT_ALIGNMENT_VIEW_END
+                else -> defaultValue
+            }
+        }
+
+        fun tryParseTextStyle(string: String?, defaultValue: Int = Typeface.NORMAL) : Int{
+            string ?:  return defaultValue
+            return when(string) {
+                "normal" -> Typeface.NORMAL
+                "bold" -> Typeface.BOLD
+                "italic" -> Typeface.ITALIC
+                "bold_italic" -> Typeface.BOLD_ITALIC
+                else -> defaultValue
+            }
+        }
+
+        fun tryParseTypeface(string: String?, defaultValue: Int = Typeface.NORMAL) : Typeface{
+            val defaultTypeface = Typeface.create(Typeface.DEFAULT, defaultValue)
+            string ?: defaultTypeface
+            return when(string) {
+                "default" -> Typeface.create(Typeface.DEFAULT, defaultValue)
+                "monospace" -> Typeface.create(Typeface.MONOSPACE, defaultValue)
+                "sans_serif"-> Typeface.create(Typeface.SANS_SERIF, defaultValue)
+                "serif" -> Typeface.create(Typeface.SERIF, defaultValue)
+                else -> defaultTypeface
+            }
+        }
+
+        fun tryParseAutolink(string: String?, defaultValue: Int = AUTOLINK_NONE) : Int{
+            string ?: return defaultValue
+            return when(string){
+                "all" -> AUTOLINK_ALL
+                "map" -> AUTOLINK_MAP
+                "phone" -> AUTOLINK_PHONE
+                "email" -> AUTOLINK_EMAIL
+                "web" -> AUTOLINK_WEB
+                "none" -> AUTOLINK_NONE
+                else -> AUTOLINK_ALL    //for multiple link types piped together
+            }
+        }
+
+        fun tryParseAutoSizeTextType(string: String? , defaultValue: Int = AUTOSIZETEXTYPE_NONE) : Int {
+            string ?: return defaultValue
+            return when (string){
+                "none" -> AUTOSIZETEXTYPE_NONE
+                "uniform" -> AUTOSIZETEXTTYPE_UNIFORM
+                else -> defaultValue
+            }
+        }
+
+        fun tryParseCaptialize
+                (string: String?, defaultValue: TextKeyListener.Capitalize = TextKeyListener.Capitalize.NONE): TextKeyListener.Capitalize {
+            string ?: return defaultValue
+            return when(string){
+                "characters" -> TextKeyListener.Capitalize.CHARACTERS
+                "words" -> TextKeyListener.Capitalize.WORDS
+                "sentences" -> TextKeyListener.Capitalize.SENTENCES
+                "none" -> TextKeyListener.Capitalize.NONE
+                else -> defaultValue
+            }
+        }
+
+        fun tryParseBreakStrategy(string: String?, defaultValue: Int = BREAKSTRATEGY_SIMPLE) : Int {
+            string ?: return defaultValue
+            return when(string){
+                "balanced" -> BREAKSTRATEGY_BALANCED
+                "high_quality" -> BREAKSTRATEGY_HIGH_QUAL
+                "simple" -> BREAKSTRATEGY_SIMPLE
+                else -> defaultValue
+            }
+        }
+
+        fun tryParseBufferType
+                (string: String?, defaultValue: TextView.BufferType = TextView.BufferType.NORMAL) : TextView.BufferType{
+            string ?: return defaultValue
+            return when(string) {
+                "editable" -> TextView.BufferType.EDITABLE
+                "spannable" -> TextView.BufferType.SPANNABLE
+                "normal" -> TextView.BufferType.NORMAL
+                else -> defaultValue
+            }
+        }
+
+        fun tryParseDrawableTintMode(string: String?, defaultValue: PorterDuff.Mode = PorterDuff.Mode.SRC_IN ) : PorterDuff.Mode {
+            string ?: return defaultValue
+            return when(string){
+                "add" -> PorterDuff.Mode.ADD
+                "multiply" -> PorterDuff.Mode.MULTIPLY
+                "screen" -> PorterDuff.Mode.SCREEN
+                "src_atop" -> PorterDuff.Mode.SRC_ATOP
+                "src_over" -> PorterDuff.Mode.SRC_OVER
+                "src_in" -> PorterDuff.Mode.SRC_IN
+                else -> defaultValue
+            }
+        }
+
+        fun tryParseEllipsize(string : String?, defaultValue: TextUtils.TruncateAt? = null) : TextUtils.TruncateAt? {
+            string ?: return defaultValue
+            return when(string){
+                "end" -> TextUtils.TruncateAt.END
+                "start" -> TextUtils.TruncateAt.START
+                "middle" -> TextUtils.TruncateAt.MIDDLE
+                "marquee" -> TextUtils.TruncateAt.MARQUEE
+                else -> null
+            }
+        }
+
+        fun tryParseGravity(string: String?, defaultValue: Int = (Gravity.TOP.or(Gravity.START))) : Int {
+            string ?: return defaultValue
+            var gravity = 0
+
+            for (param in split(string,"|")){
+                when(param){
+                    "top" -> gravity.or(Gravity.TOP)
+                    "bottom" -> gravity.or(Gravity.BOTTOM)
+                    "center" -> gravity.or(Gravity.CENTER)
+                    "center_horizontal" -> gravity.or(Gravity.CENTER_HORIZONTAL)
+                    "center_vertical" -> gravity.or(Gravity.CENTER_VERTICAL)
+                    "clip_horizontal" -> gravity.or(Gravity.CLIP_HORIZONTAL)
+                    "clip_vertical" -> gravity.or(Gravity.CLIP_VERTICAL)
+                    "end" -> gravity.or(Gravity.END)
+                    "fill" -> gravity.or(Gravity.FILL)
+                    "fill_horizontal" -> gravity.or(Gravity.FILL_HORIZONTAL)
+                    "fill_vertical" -> gravity.or(Gravity.FILL_VERTICAL)
+                    "left" -> gravity.or(Gravity.LEFT)
+                    "right" -> gravity.or(Gravity.RIGHT)
+                    "start" -> gravity.or(Gravity.START)
+                    else -> gravity
+                }
+            }
+            return gravity
+        }
+
+        fun tryParseHyphenationFrequency(string: String?, defaultValue: Int = HYPHENATIONFREQ_NONE): Int {
+            string ?: return defaultValue
+            return when(string){
+                "full" -> HYPHENATIONFREQ_FULL
+                "normal" -> HYPHENATIONFREQ_NORMAL
+                "none" -> HYPHENATIONFREQ_NONE
+                else -> defaultValue
+            }
+        }
+
+        fun tryParseImeOptions(string: String?, defaultValue: Int = EditorInfo.IME_NULL): Int{
+            string ?: return defaultValue
+            var imeOptions = 0
+            for (option in split(string,"|")){
+                imeOptions.or(when(option){
+                    "actionDone" -> EditorInfo.IME_ACTION_DONE
+                    "actionGo" -> EditorInfo.IME_ACTION_GO
+                    "actionNext"-> EditorInfo.IME_ACTION_NEXT
+                    "actionNone" -> EditorInfo.IME_ACTION_NONE
+                    "actionPrevious" -> EditorInfo.IME_ACTION_PREVIOUS
+                    "actionSearch" -> EditorInfo.IME_ACTION_SEARCH
+                    "actionSend" -> EditorInfo.IME_ACTION_SEND
+                    "actionUnspecified" -> EditorInfo.IME_ACTION_UNSPECIFIED
+                    "flagForceAscii" -> EditorInfo.IME_FLAG_FORCE_ASCII
+                    "flagNavigateNext" -> EditorInfo.IME_FLAG_NAVIGATE_NEXT
+                    "flagNavigatePrevious" -> EditorInfo.IME_FLAG_NAVIGATE_PREVIOUS
+                    "flagNoAccessoryAction" -> EditorInfo.IME_FLAG_NO_ACCESSORY_ACTION
+                    "flagNoEnterAction" -> EditorInfo.IME_FLAG_NO_ENTER_ACTION
+                    "flagNoExtractUI" -> EditorInfo.IME_FLAG_NO_EXTRACT_UI
+                    "flagNoFullScreen" -> EditorInfo.IME_FLAG_NO_FULLSCREEN
+                    "flagNoPersonalizedLearning" -> EditorInfo.IME_FLAG_NO_PERSONALIZED_LEARNING
+                    "normal" -> EditorInfo.IME_NULL
+                    else -> defaultValue
+                })
+            }
+            return imeOptions
+        }
+
+        fun tryParseInputType(string: String?, defaultValue: Int = InputType.TYPE_NULL) : Int {
+            string ?: return defaultValue
+            val inputType = 0
+            for (type in split(string, "|")){
+                inputType.or(when(type){
+                    "date" -> InputType.TYPE_CLASS_DATETIME.or(InputType.TYPE_DATETIME_VARIATION_DATE)
+                    "datetime" -> InputType.TYPE_CLASS_DATETIME.or(InputType.TYPE_DATETIME_VARIATION_NORMAL)
+                    "decimal" -> InputType.TYPE_NUMBER_FLAG_DECIMAL
+                    "integer" -> InputType.TYPE_CLASS_NUMBER
+                    "none" -> InputType.TYPE_NULL
+                    "number" -> InputType.TYPE_CLASS_NUMBER.or(InputType.TYPE_NUMBER_VARIATION_NORMAL)
+                    "numberDecimal" -> InputType.TYPE_CLASS_NUMBER.or(InputType.TYPE_NUMBER_FLAG_DECIMAL)
+                    "numberPassword" -> InputType.TYPE_CLASS_NUMBER.or(InputType.TYPE_NUMBER_VARIATION_PASSWORD)
+                    "numberSigned" -> InputType.TYPE_CLASS_NUMBER.or(InputType.TYPE_NUMBER_FLAG_SIGNED)
+                    "phone" -> InputType.TYPE_CLASS_PHONE
+                    "text" -> InputType.TYPE_CLASS_TEXT.or(InputType.TYPE_TEXT_VARIATION_NORMAL)
+                    "textAutoComplete" -> InputType.TYPE_TEXT_FLAG_AUTO_COMPLETE
+                    "textAutoCorrect" -> InputType.TYPE_TEXT_FLAG_AUTO_CORRECT
+                    "textCapCharacters" -> InputType.TYPE_TEXT_FLAG_CAP_CHARACTERS
+                    "textCapSentences" -> InputType.TYPE_TEXT_FLAG_CAP_SENTENCES
+                    "textCapWords" -> InputType.TYPE_TEXT_FLAG_CAP_WORDS
+                    "textEmailAddress" -> InputType.TYPE_CLASS_TEXT.or(InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS)
+                    "textEmailSubject" -> InputType.TYPE_CLASS_TEXT.or(InputType.TYPE_TEXT_VARIATION_EMAIL_SUBJECT)
+                    "textFilter" -> InputType.TYPE_CLASS_TEXT.or(InputType.TYPE_TEXT_VARIATION_FILTER)
+                    "textImeMultiLine" -> InputType.TYPE_TEXT_FLAG_IME_MULTI_LINE
+                    "textLongMessage" -> InputType.TYPE_CLASS_TEXT.or(InputType.TYPE_TEXT_VARIATION_LONG_MESSAGE)
+                    "textMultiLine" -> InputType.TYPE_TEXT_FLAG_IME_MULTI_LINE
+                    "textNoSuggestions" -> InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS
+                    "textPassword" -> InputType.TYPE_CLASS_TEXT.or(InputType.TYPE_TEXT_VARIATION_PASSWORD)
+                    "textPersonName" -> InputType.TYPE_CLASS_TEXT.or(InputType.TYPE_TEXT_VARIATION_PERSON_NAME)
+                    "textPhonetic" -> InputType.TYPE_CLASS_TEXT.or(InputType.TYPE_TEXT_VARIATION_PHONETIC)
+                    "textPostalAddress" -> InputType.TYPE_CLASS_TEXT.or(InputType.TYPE_TEXT_VARIATION_POSTAL_ADDRESS)
+                    "textShortMessage" -> InputType.TYPE_CLASS_TEXT.or(InputType.TYPE_TEXT_VARIATION_SHORT_MESSAGE)
+                    "textUri" -> InputType.TYPE_CLASS_TEXT.or(InputType.TYPE_TEXT_VARIATION_URI)
+                    "textVisiblePassword" -> InputType.TYPE_CLASS_TEXT.or(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD)
+                    "textWebEditText" -> InputType.TYPE_CLASS_TEXT.or(InputType.TYPE_TEXT_VARIATION_WEB_EDIT_TEXT)
+                    "textWebEmailAddress" -> InputType.TYPE_CLASS_TEXT.or(InputType.TYPE_TEXT_VARIATION_WEB_EMAIL_ADDRESS)
+                    "textWebPassword" -> InputType.TYPE_CLASS_TEXT.or(InputType.TYPE_TEXT_VARIATION_WEB_PASSWORD)
+                    "time" -> InputType.TYPE_CLASS_DATETIME.or(InputType.TYPE_DATETIME_VARIATION_TIME)
+                    "signed" -> InputType.TYPE_NUMBER_FLAG_SIGNED
+                    else -> defaultValue
+                })
+            }
+            return inputType
+        }
+
+        fun tryParseMarqueeRepeatLimit(string: String?, defaultValue: Int = 0) : Int{
+            string ?: return defaultValue
+            return when (string){
+                "marquee_forever" -> MARQUEEREPEAT_FOREVER
+                else -> defaultValue
+            }
+        }
+
+        fun tryParseResourceId(string: String?, defaultValue: Int = 0) : Int{
+            string ?: return defaultValue
+            if (!string.contains("@") || !string.contains(":") || !string.contains("/")) return defaultValue
+
+            var packageStr = string.substring(string.indexOf("@") + 1, string.indexOf(":"))
+            var typeStr = string.substring(string.indexOf(":") + 1, string.indexOf("/"))
+            var nameStr = string.substring(string.indexOf("/") + 1, string.length)
+
+            return Resources.getSystem().getIdentifier(nameStr, typeStr, packageStr)
+        }
+
+        fun tryParseInputFilter(string: String?, defaultValue: Array<InputFilter> = emptyArray()): Array<InputFilter> {
+            string ?: return defaultValue
+            val maxLength = string.toIntOrNull()
+            return when (maxLength){
+                is Int -> arrayOf(InputFilter.LengthFilter(maxLength))
                 else -> defaultValue
             }
         }
